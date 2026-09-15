5e2637442c09503b1bb4429e183fad189e0b5573
Maggie1232101 <1466677@learn.vsb.bc.ca>
2026-09-15 14:49:51 -0700

    Add files via upload

--- time.java
+++ time.java
@@ -1,8 +1,10 @@
 public class time{
 	public static void main(String[] args){
+		
 		int hour = 14;
 		int minute = 36;
 		int second = 22;
+		
 		System.out.println("Time since midnight: " + hour + "h" + minute + "m" + second + "s");
 		System.out.print("Seconds since midnight: ");
 		System.out.println((hour*60*60) + (minute*60) + second);
@@ -12,7 +14,11 @@ public class time{
 		System.out.println(60-second + "s");
 		System.out.print("Percent of day complete: ");
 		System.out.println((hour*60+minute)*100/1440 + "%");
-		
+
+		hour = 14;
+		minute = 44;
+		second = 31;
+		System.out.print("Elapsed Time: " + ((hour*60*60+minute*60 + second)-(14*60*60 + 36*60 + 22))+ "seconds");
 	
 	
 	
